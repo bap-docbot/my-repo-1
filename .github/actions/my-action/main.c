1edfc749a934261701af2f2d3de0b1acb71b5fdd
Oleg <forown@yandex.ru>
2020-03-03 10:23:21 -0500

    updates ..

---
+++ .github/actions/my-action/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void create_product(const char *x) {
+    FILE *f = fopen("product", "w");
+    fputs(x,f);
+    fputc('\n', f);
+    fclose(f);
+}
+
+int main(int n, const char ** argv) {
+    if (n == 1) {
+        const char a[] = "anon";
+        printf("Hello world, %s\n", a);
+        create_product(a);
+    } else {
+        printf("Hello world, %s\n", argv[1]);
+        create_product(argv[1]);
+    };
+
+}
