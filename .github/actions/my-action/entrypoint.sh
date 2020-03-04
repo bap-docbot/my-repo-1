a779b093b8f5ac44441449c788a11354437e164d
Oleg <forown@yandex.ru>
2020-03-04 15:43:33 -0500

    dd

--- .github/actions/my-action/entrypoint.sh
+++ .github/actions/my-action/entrypoint.sh
@@ -4,8 +4,8 @@ git clone https://github.com/gitoleg/my-repo-2
 cd my-repo-2
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_MYSECRET}@github.com/gitoleg/my-repo-2.git"
 
-#git config --global user.name ${GITHUB_ACTOR}
-#git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
+git config --global user.name ${GITHUB_ACTOR}
+git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
 
 git remote set-url origin $remote_repo
 
@@ -17,4 +17,4 @@ git commit -m "$msg"
 echo "git remote -v"
 git remote -v
 
-git push my-remote master
+git push origin master
