6b053816007a71333ea6bea79768a6f0b5771489
Oleg <forown@yandex.ru>
2020-03-04 15:24:39 -0500

    aa

--- .github/actions/my-action/entrypoint.sh
+++ .github/actions/my-action/entrypoint.sh
@@ -2,18 +2,19 @@
 
 git clone https://github.com/gitoleg/my-repo-2
 cd my-repo-2
-remote_repo="https://${GITHUB_ACTOR}:${1}@github.com/my-repo-2.git"
-echo "setting remote"
+remote_repo="https://${GITHUB_ACTOR}:${INPUT_MYSECRET}@github.com/my-repo-2.git"
+
 git config --global user.name ${GITHUB_ACTOR}
 git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
-git
 
+git remote add my-remote $remote_repo
 
-git remote set-url --push origin $remote_repo
 cp /product .
 msg=`git rev-parse --short HEAD`
 git add product
 git commit -m "$msg"
+
+echo "git remote -v"
 git remote -v
 
-git push
+git push my-remote master
