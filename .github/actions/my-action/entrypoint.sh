73d597249b548cae987d92d64944863a49ece999
Oleg <forown@yandex.ru>
2020-03-04 13:19:40 -0500

    try

--- .github/actions/my-action/entrypoint.sh
+++ .github/actions/my-action/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-cp product $1
+if [ "no$1" != "no" ]; then
+    echo "has a secret"
+else
+    echo "has NO secret"
+fi
+
+git clone https://github.com/gitoleg/my-repo-2
+cd my-repo-2
+cp product .
+msg=`git rev-parse --short HEAD`
+git commit -m "$msg"
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/my-repo-2.git"
+git push "${remote_repo}" master
