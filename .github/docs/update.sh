842f103409fe69d4c8b42b0bd34691f18b501deb
Oleg <forown@yandex.ru>
2020-03-10 16:14:58 -0400

    test

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -5,10 +5,12 @@ TOKEN=$1
 repo="github.com/gitoleg/my-repo-1"
 remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@${repo}.git"
 
-git clone $repo
+git clone https://$repo
 cd my-repo-1
 msg=`git rev-parse --short HEAD`
-touch $RANDOM
+touch 1
+git config --global user.name $GITHUB_ACTOR
+git config --global user.email norelpy@github.com
 
 git add .
 git commit -m "$msg"
