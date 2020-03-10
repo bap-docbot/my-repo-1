7e4366ffe1eb9f4220f910232f307f87da53356b
Oleg <forown@yandex.ru>
2020-03-10 17:03:58 -0400

    test

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -3,7 +3,7 @@
 TOKEN=$1
 
 repo="github.com/gitoleg/my-repo-1"
-remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@${GITHUB_REPOSITORY}.git"
+remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 msg=`git rev-parse --short HEAD`
 touch 4
@@ -13,3 +13,4 @@ git config --global user.email "noreply@github.com"
 git add .
 git commit -m "$msg"
 git push $remote_repo master
+last=`git log --pretty=format:"%s" | head -n 1`
