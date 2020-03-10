3bbf43da6a66cfaa19579bafdd7d11c48ed1fcc1
Oleg <forown@yandex.ru>
2020-03-10 16:02:19 -0400

    debug

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -3,9 +3,11 @@
 check () {
     if [ "no$2" = "no" ]; then
         echo "noenv $1"
+    else
+        echo "has $1"
     fi
 }
 
 echo "WE ARE HERE "
 check "actor" $GITHUB_ACTOR
-check "token" $GITHUB_TOKEN
+check "token" $INPUT_TOKEN
