ab4d99136871a4a4dcb4dc4ed4ab89e1d289b650
Oleg <forown@yandex.ru>
2020-03-10 16:48:27 -0400

    test

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -2,12 +2,16 @@
 
 TOKEN=$1
 
-if [ "no$TOKEN" != "no" ]; then
-    echo "has TOKEN!!!"
-else
-    echo "NOTOKEN!!!"
-
-fi
+check () {
+    if [ "no$2" != "no" ]; then
+        echo "has $1!!!"
+    else
+        echo "NO $1!!!"
+    fi
+}
+check "TOKEN" $1
+check "GITHUB_TOKEN" $GITHUB_TOKEN
+check "INPUT_GITHUB_TOKEN" $INPUT_GITHUB_TOKEN
 
 repo="github.com/gitoleg/my-repo-1"
 remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@${repo}.git"
