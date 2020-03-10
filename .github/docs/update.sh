64812a4ff1d1a418423aea88b81dd6a2810979ad
Oleg <forown@yandex.ru>
2020-03-10 17:06:14 -0400

    test

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -6,9 +6,9 @@ repo="github.com/gitoleg/my-repo-1"
 remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 msg=`git rev-parse --short HEAD`
-touch 4
+touch 5
 git config --global user.name $GITHUB_ACTOR
-git config --global user.email "noreply@github.com"
+git config --global user.email "forown@yandex.ru"
 
 git add .
 git commit -m "$msg"
