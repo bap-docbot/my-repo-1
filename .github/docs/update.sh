06dd76f7ab3faeef30c7f5042b6021f8317c3784
Oleg <forown@yandex.ru>
2020-03-11 16:23:37 -0400

    aa

--- .github/docs/update.sh
+++ .github/docs/update.sh
@@ -1,22 +1,4 @@
 #!/usr/bin/env sh
 
-TOKEN=$1
-
-remote_repo="https://${GITHUB_ACTOR}:${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-
-msg=`git rev-parse --short HEAD`
-git config --global user.name $GITHUB_ACTOR
-git config --global user.email "action-noreply@github.com"
-
-x=`od -An -N1 -i /dev/random`
-y=`echo $x`
-touch $y
-
-current=`git rev-parse --short HEAD`
-last=`git log --pretty=format:"%s" | head -n 1`
-echo "last is $last, current $current"
-
-
-git add .
-git commit -m "$msg"
-git push $remote_repo master
+echo aaa > myfile
+cp myfile /drive
